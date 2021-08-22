2b617cbb12a5edca7bc9d4eb0e48a27d8d19a8cd
damario789 <damario789@gmail.com>
2021-08-22 22:11:45 +0700

    tambahan di apollo provider di index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from "@apollo/client";
+import client from "./config/apollo";
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
