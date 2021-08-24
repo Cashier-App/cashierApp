64f36392d6411b12f7543b56d0ff7398bca6b41d
damario789 <damario789@gmail.com>
2021-08-24 12:40:20 +0700

    handle edit stock item

--- client/src/config/StockItem.js
+++ client/src/config/StockItem.js
@@ -47,7 +47,7 @@ export const ADD_STOCK_ITEM_MUTATION = gql`
 `;
 
 export const UPDATE_STOCK_ITEM_MUTATION = gql`
-mutation EditStockItem($_id: ID, $file: Upload!, $name: String, $price: Float, $category: String, $recipes: [add_recipe], $stock: Float) {
+mutation AddStockItemMutation($_id: ID, $file: Upload, $name: String, $price: Float, $category: String, $recipes: [add_recipe], $stock: Float) {
   editStockItem(_id: $_id, file: $file, name: $name, price: $price, category: $category, recipes: $recipes, stock: $stock) {
     _id
     name
@@ -67,9 +67,10 @@ mutation EditStockItem($_id: ID, $file: Upload!, $name: String, $price: Float, $
       }
       qty
     }
+    stock
   }
 }
-`
+`;
 
 export const FETCH_ALL_STOCK_ITEM = gql`
   query FetchStockItems {
