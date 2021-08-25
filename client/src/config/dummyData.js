4ae137086fe4b1ea6639dbb2f80f8a780fa908aa
damario789 <damario789@gmail.com>
2021-08-25 18:59:58 +0700

    mau di merge

---
+++ client/src/config/dummyData.js
@@ -0,0 +1,128 @@
+export const sales = [
+  {
+    _id: "612544c0ca9e67e80c303f71",
+    items: [
+      {
+        _id: "612544c0ca9e67e80c303f72",
+        item: {
+          _id: "612521e5393142cca19da863",
+          name: "Bakmi Kelinci",
+          price: 100000,
+          category: {
+            _id: "61251153cadee3b257210720",
+            name: "Food",
+          },
+          imageUrl: "https://ik.imagekit.io/damario789/1629823451388--local-file-output12_fJThdTVbg.jpg",
+          recipes: [
+            {
+              _id: "612521e5393142cca19da864",
+              ingredient: {
+                _id: "61251170cadee3b257210723",
+                name: "Mie",
+                unit: "Pack",
+                total: 10,
+              },
+              qty: 2,
+            },
+          ],
+          stock: 5,
+        },
+        qty: 5,
+      },
+      {
+        _id: "612544c0ca9e67e80c303f72",
+        item: {
+          _id: "612521e5393142cca19da863",
+          name: "Bakmi Kelinci",
+          price: 100000,
+          category: {
+            _id: "61251153cadee3b257210720",
+            name: "Food",
+          },
+          imageUrl: "https://ik.imagekit.io/damario789/1629823451388--local-file-output12_fJThdTVbg.jpg",
+          recipes: [
+            {
+              _id: "612521e5393142cca19da864",
+              ingredient: {
+                _id: "61251170cadee3b257210723",
+                name: "Mie",
+                unit: "Pack",
+                total: 10,
+              },
+              qty: 2,
+            },
+          ],
+          stock: 5,
+        },
+        qty: 2,
+      },
+    ],
+    payment: "Debit",
+    total: 700000,
+    adminName: "Abdel",
+    date: "2021-08-24T18:43:58.962Z",
+  },
+  {
+    _id: "612544c0ca9e67e80c303f71",
+    items: [
+      {
+        _id: "612544c0ca9e67e80c303f72",
+        item: {
+          _id: "612521e5393142cca19da863",
+          name: "Bakmi Kelinci",
+          price: 100000,
+          category: {
+            _id: "61251153cadee3b257210720",
+            name: "Food",
+          },
+          imageUrl: "https://ik.imagekit.io/damario789/1629823451388--local-file-output12_fJThdTVbg.jpg",
+          recipes: [
+            {
+              _id: "612521e5393142cca19da864",
+              ingredient: {
+                _id: "61251170cadee3b257210723",
+                name: "Mie",
+                unit: "Pack",
+                total: 10,
+              },
+              qty: 2,
+            },
+          ],
+          stock: 5,
+        },
+        qty: 5,
+      },
+      {
+        _id: "612544c0ca9e67e80c303f72",
+        item: {
+          _id: "612521e5393142cca19da863",
+          name: "Bakmi Kelinci",
+          price: 100000,
+          category: {
+            _id: "61251153cadee3b257210720",
+            name: "Food",
+          },
+          imageUrl: "https://ik.imagekit.io/damario789/1629823451388--local-file-output12_fJThdTVbg.jpg",
+          recipes: [
+            {
+              _id: "612521e5393142cca19da864",
+              ingredient: {
+                _id: "61251170cadee3b257210723",
+                name: "Mie",
+                unit: "Pack",
+                total: 10,
+              },
+              qty: 2,
+            },
+          ],
+          stock: 5,
+        },
+        qty: 2,
+      },
+    ],
+    payment: "Debit",
+    total: 700000,
+    adminName: "Abdel",
+    date: "2021-08-24T18:43:58.962Z",
+  },
+];
